8cabd6adf5f303303a84469e0993f6203bd415a5
Filipe Parente <lfmparente@gmail.com>
2020-09-12 01:07:11 +0200

    34. Pre and Postfix Increment and Decrement Operators

--- src/section4/UnaryOperator.java
+++ src/section4/UnaryOperator.java
@@ -42,6 +42,46 @@ public class UnaryOperator {
     int d1 = ~d;
     System.out.println(d1);
 
+    // What happens if we do this?
+    System.out.println("----------Test 2 ----------");
+    a = 1;
+    System.out.println("a after ++a = " + ++a);
+    a = 1;
+    System.out.println("a after a++ = " + a++);
+    System.out.println("And now the value of a is: " + a);
+
+    System.out.println("----------Test 3 ----------");
+    // Let's look at postfix increment in a variable declaration
+    a = 1;
+    int a2 = a++;
+    System.out.println("The value of a is " + a);
+    System.out.println("The value of a2 is " + a2);
+
+    // Let's look at postfix in an expression...
+    a = 1;
+    if (++a == 1) {
+      System.out.println("If Statement, now the value of a is " + a);
+    }
+    System.out.println("----------End 3 ----------");
+
+    System.out.println("----------Test 4 ----------");
+    int b2 = 5;
+    int loopiterations = 0;
+    while (--b2 > 0) { // Use a prefix decrement
+      loopiterations++;
+    }
+    System.out.println(
+        "Prefix decrement operator used, loopiterations = " + loopiterations + ", b = " + b2);
+
+    // Reset values, all things equal except the type of increment operator
+    b2 = 5;
+    loopiterations = 0;
+    while (b2-- > 0) { // Use a postfix decrement
+      loopiterations++;
+    }
+    System.out.println(
+        "Postfix decrement operator used, loopiterations = " + loopiterations + ", b = " + b2);
+
 
   }
 
